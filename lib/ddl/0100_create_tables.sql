e20d04e1eb5470cc0d0f0dfce4270720cbc0e968
Paul Water <p.r.water@setbased.nl>
2017-01-10 10:56:29 +0100

    Added DDL script.

---
+++ lib/ddl/0100_create_tables.sql
@@ -0,0 +1,95 @@
+/*================================================================================*/
+/* DDL SCRIPT                                                                     */
+/*================================================================================*/
+/*  Title    :                                                                    */
+/*  FileName : abc-blob.ecm                                                       */
+/*  Platform : MySQL 5.6                                                          */
+/*  Version  : Concept                                                            */
+/*  Date     : dinsdag 10 januari 2017                                            */
+/*================================================================================*/
+/*================================================================================*/
+/* CREATE TABLES                                                                  */
+/*================================================================================*/
+
+CREATE TABLE ABC_BLOB_DATA (
+  bdt_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  cmp_id SMALLINT UNSIGNED NOT NULL,
+  bdt_md5 BINARY(16) NOT NULL,
+  bdt_data LONGBLOB NOT NULL,
+  CONSTRAINT PRIMARY_KEY PRIMARY KEY (bdt_id)
+)
+PARTITION BY RANGE (bdt_id)
+(
+  PARTITION `p00000` VALUES LESS THAN MAXVALUE
+);
+
+/*
+COMMENT ON COLUMN ABC_BLOB_DATA.bdt_md5
+The MD5 hash of the BLOB.
+*/
+
+/*
+COMMENT ON COLUMN ABC_BLOB_DATA.bdt_data
+The actual BLOB.
+*/
+
+CREATE TABLE ABC_BLOB (
+  blb_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
+  cmp_id SMALLINT UNSIGNED NOT NULL,
+  bdt_id INT UNSIGNED NOT NULL,
+  blb_size INT UNSIGNED NOT NULL,
+  blb_md5 BINARY(16) NOT NULL,
+  blb_filename VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  blb_mime_type VARCHAR(48) NOT NULL,
+  blb_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  CONSTRAINT PRIMARY_KEY PRIMARY KEY (blb_id)
+);
+
+/*
+COMMENT ON COLUMN ABC_BLOB.blb_size
+The size in bytes of the BLOB.
+*/
+
+/*
+COMMENT ON COLUMN ABC_BLOB.blb_md5
+The MD5 hash of the BLOB.
+*/
+
+/*
+COMMENT ON COLUMN ABC_BLOB.blb_filename
+The name of the BLOB.
+*/
+
+/*
+COMMENT ON COLUMN ABC_BLOB.blb_mime_type
+The MIME type of the BLOB.
+*/
+
+/*
+COMMENT ON COLUMN ABC_BLOB.blb_timestamp
+The timestamp when the BLOB was inserted or when known the last modification time of the BLOB.
+*/
+
+/*================================================================================*/
+/* CREATE INDEXES                                                                 */
+/*================================================================================*/
+
+CREATE INDEX bdt_md5 ON ABC_BLOB_DATA (bdt_md5);
+
+CREATE INDEX cmp_id ON ABC_BLOB_DATA (cmp_id);
+
+CREATE INDEX IX_ABC_BLOB1 ON ABC_BLOB (blb_md5);
+
+/*================================================================================*/
+/* CREATE FOREIGN KEYS                                                            */
+/*================================================================================*/
+
+ALTER TABLE ABC_BLOB
+  ADD CONSTRAINT FK_ABC_BLOB_ABC_BLOB_DATA
+  FOREIGN KEY (bdt_id) REFERENCES ABC_BLOB_DATA (bdt_id)
+  ON UPDATE NO ACTION
+  ON DELETE NO ACTION;
+
+ALTER TABLE ABC_BLOB
+  ADD CONSTRAINT FK_ABC_BLOB_AUT_COMPANY
+  FOREIGN KEY (cmp_id) REFERENCES AUT_COMPANY (cmp_id);
